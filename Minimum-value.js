882bf9fa864b935543b8579af0d71d56f9e128fa
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-01-17 00:03:38 +0700

    tim gia tri nho nhat trong mang

---
+++ Minimum-value.js
@@ -0,0 +1,19 @@
+let arr1 = [3, 5, 1, 8, -3, 7, 8];
+let arr2 = [7, 12, 6, 9, 20, 56, 89];
+let arr3 = [];
+let arr4 = [0, 0, 0, 0, 0, 0];
+let arrNum = [arr1, arr2, arr3, arr4];
+
+function minArray(arr) {
+    let min = arr[0];
+    if (arr.length == 0) {
+        return -1;
+    }
+    for (let i = 1; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
